373ebec841d52266fe57347e540374824f5ff6be
Oscar Evertsson <oscar@arkkapital.com>
2023-04-08 15:47:00 +0200

    Add comments

--- src/source/launch.py
+++ src/source/launch.py
@@ -1,10 +1,11 @@
 import importlib
 from typing import Callable
+
 import uvicorn
 from fastapi import FastAPI
 
-from src.source.source import Source
 from src.source.routes import register_routes
+from src.source.source import Source
 
 app = FastAPI()
 
@@ -14,6 +15,8 @@ def register(get_source: Callable[[], Source]):
 
 
 def start():
+    # TODO: Add support for sending in configuration when starting the server to the source
+    # TODO: Take source module as an argument on startup
     example_module = importlib.import_module("src.source.example.example")
     get_source: Callable[[], Source] = getattr(example_module, "create")
     register(get_source=get_source)
